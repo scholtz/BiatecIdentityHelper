ce78eee1fb466a0f441464f7bae04127bd6cbddc
Scholtz <ludko2@gmail.com>
2024-12-26 08:51:43 +0100

    k8s config

--- k8s/main/update-config.sh
+++ k8s/main/update-config.sh
@@ -1,5 +1,12 @@
-kubectl apply -f deployment.yaml -n biatec-identity
+kubectl apply -f deployment-1.yaml -n biatec-identity
 kubectl delete configmap biatec-identity-helper-1-conf -n biatec-identity
 kubectl create configmap biatec-identity-helper-1-conf --from-file=conf -n biatec-identity
 kubectl rollout restart deployment/biatec-identity-helper-1-app-deployment -n biatec-identity
+
+kubectl apply -f deployment-2.yaml -n biatec-identity
+kubectl delete configmap biatec-identity-helper-2-conf -n biatec-identity
+kubectl create configmap biatec-identity-helper-2-conf --from-file=conf -n biatec-identity
+kubectl rollout restart deployment/biatec-identity-helper-2-app-deployment -n biatec-identity
+
 kubectl rollout status deployment/biatec-identity-helper-1-app-deployment -n biatec-identity
+kubectl rollout status deployment/biatec-identity-helper-2-app-deployment -n biatec-identity
